69a53a407e2f6db2930fed82abc9df353cf2b775
kathlind <nikkinnik93@gmail.com>
2026-05-17 07:10:42 +0300

    c

--- Formula/s21check.rb
+++ Formula/s21check.rb
@@ -1,7 +1,7 @@
 class S21check < Formula
   desc "School 21 C project checker"
-  homepage "https://github.com/s21toolkit/s21check"
-  url "https://github.com/s21toolkit/s21check/archive/refs/tags/v1.0.0.tar.gz"
+  homepage "https://github.com/s21-tools/s21check"
+  url "https://github.com/s21-tools/s21check/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
 
